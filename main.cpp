7305f553d5b262c516f4ac8faaf2628b0a25eaae
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-12-03 11:15:23 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
-  return 0;
+    int n=0,c=2;
+    cin>>n;
+    if(numprimo(n,c)==true){
+        cout <<"numero primo";
+    }
+    else{
+        cout <<"numero non primo";
+    }
+    return 0;
 }
