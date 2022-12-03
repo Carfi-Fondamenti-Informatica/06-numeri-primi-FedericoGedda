d65b8788093d1632a23a8597c6a13cabad6cb015
FedericoGedda <115471970+FedericoGedda@users.noreply.github.com>
2022-12-03 11:18:14 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
-
+#include "lib.h"
+bool n_primo(int n, int c){
+    if(n%c==0){
+        if(n==c){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    numprimo(n,c+1);
+}
